eb40cfc34e3f51b7ab1466311a8b333d986554ca
JuRyeon33 <thwnfus0330@gmail.com>
2025-06-18 10:16:29 +0900

    List fruits remove

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Main {
@@ -11,12 +12,21 @@ public class Main {
         fruits.add("Dragonfruit");
         fruits.add("Mango");
 
+        //fruits.remove("Banana");
+        fruits.remove(1);
+
        // for(int i = 0; i < fruits.size(); i++) {
         //    System.out.println(fruits.get(i));
         //}
 
+        Collections.sort(fruits);
+
         for (String fruit : fruits) {
             System.out.println(fruit);
+        }
+
+        if (fruits.contains("Apple")) {
+            System.out.println("Apple이 목록에 있습니다.");
         }
     }
 }
